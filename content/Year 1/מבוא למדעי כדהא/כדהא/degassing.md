62027fd327b996e1366ff2c3a8887b530f30ca8d
YPG <yuval.pele@gmail.com>
2025-01-27 18:10:05 +0200

    Quartz sync: Jan 27, 2025, 6:10 PM

--- content/Year 1/מבוא למדעי כדהא/כדהא/degassing.md
+++ content/Year 1/מבוא למדעי כדהא/כדהא/degassing.md
@@ -3,5 +3,6 @@ cssclass: rtl-class
 draft: false
 title: 
 description: 
+publish: true
 ---
 תהליך בו הרי געש פלטו גזים מעומק [[כדור הארץ]], עם מינרלים מפורקים, וכך ייצרו [[אטמוספירה]].
--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "YYY Templates", "YYY Other Stuff"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
@@ -74,7 +74,9 @@ const config: QuartzConfig = {
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
-    filters: [Plugin.RemoveDrafts()],
+    filters: [
+	  Plugin.RemoveDrafts(),
+	  Plugin.ExplicitPublish()],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
